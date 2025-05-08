6f9808f3ce6697d3bbafd43222b89d04df06e6c5
xnought <drbertucci@hotmail.com>
2025-05-08 14:07:42 -0400

    use uint

--- isprime.go
+++ isprime.go
@@ -7,29 +7,33 @@ import (
 	"strconv"
 )
 
-func readStdInNums() []int {
+func readStdInNums() []uint64 {
 	scanner := bufio.NewScanner(os.Stdin)
-	nums := []int{}
+	nums := []uint64{}
 	for scanner.Scan() {
 		strNum := scanner.Text()
 		if len(strNum) == 0 {
 			return nums
 		}
 
-		num, err := strconv.Atoi(strNum)
+		num, err := strconv.ParseUint(strNum, 10, 64)
 		if err != nil {
-			panic("String to Integer conversion failed")
+			panic(err)
 		}
 		nums = append(nums, num)
 	}
 	return nums
 }
 
-func isEven(num int) bool {
+func isEven(num uint64) bool {
 	return (num & 1) == 0
 }
 
-func IsPrime(num int) bool {
+func IsPrime(num uint64) bool {
+	// we consider 1 prime
+	if num == 1 {
+		return true
+	}
 	// definitely isn't prime if the number is even (but 2 is only prime even)
 	if num != 2 && isEven(num) {
 		return false
@@ -37,7 +41,7 @@ func IsPrime(num int) bool {
 
 	// check if any numbers divide into num
 	// if yes, not prime!
-	for i := 2; i < num; i++ {
+	for i := uint64(2); i < num; i++ {
 		if num%i == 0 {
 			return false
 		}
